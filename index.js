9910b009b43d7ff75cd3c5919dd6df56d36f5bf1
Christian Alston <xcda15@gmail.com>
2020-07-02 00:19:43 -0400

    another

---
+++ index.js
@@ -0,0 +1,12 @@
+// fetch(
+//     "http://api.openweathermap.org/data/2.5/weather?q={Atlanta}&appid={26abbdc44231319a5158bd247b054a6c
+// }
+// ")
+// .then(response => response.json())
+//     .then(body => {
+//         document.querySelector("#temp").innerHTML = body.main.weatherbox.temp + "°";
+//         if (document.querySelector("#temp").innerHTML >= 70 && document.querySelector('#temp') < 100) {
+//             document.querySelector('#temp').innerHTML == 'its hot lol';
+//         }
+//     })
+//     .catch(error => alert("Try again"));
